5f475aef02eb3607d0ca88832605d52f51989e1c
Dariusz Walda <walda.darek@gmail.com>
2024-02-27 10:20:05 +0100

    im satisfited after long time

---
+++ Training/Python/8kyu/26_50_exercises/31_count_the_number_of_cubes_with_paint_on/count_the_number_of_cubes_with_paint_on_best_solution.py
@@ -0,0 +1,23 @@
+def count_squares(cuts):
+    outside_block = (cuts+1)**3
+    inside_block = (cuts-1)**3
+
+    return outside_block-inside_block
+
+
+test000 = count_squares(0)
+test00 = count_squares(1)
+test0a = count_squares(2)
+test0b = count_squares(4)
+test1 = count_squares(5)
+test2 = count_squares(16)
+test3 = count_squares(23)
+
+
+print(f"{test000} # 1")
+print(f"{test00} # 8")
+print(f"{test0a} # 26")
+print(f"{test0b} # 98")
+print(f"{test1} # 152")
+print(f"{test2} # 1538")
+print(f"{test3} # 3176")
