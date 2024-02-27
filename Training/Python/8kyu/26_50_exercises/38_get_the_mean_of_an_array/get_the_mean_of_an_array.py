51146bf7a7f80b035697cef72dae67850a59679c
Dariusz Walda <walda.darek@gmail.com>
2024-02-27 21:44:40 +0100

    challenge 38

---
+++ Training/Python/8kyu/26_50_exercises/38_get_the_mean_of_an_array/get_the_mean_of_an_array.py
@@ -0,0 +1,16 @@
+def get_average(marks):
+    sum = 0
+    for i in marks:
+        sum += i
+    return int(sum / len(marks))
+
+
+test1 = get_average([2, 2, 2, 2])
+test2 = get_average([1, 5, 87, 45, 8, 8])
+test3 = get_average([2,5,13,20,16,16,10])
+test4 = get_average([1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7])
+
+print(f"{test1} # 2")
+print(f"{test2} # 25")
+print(f"{test3} # 11")
+print(f"{test4} # 11")
