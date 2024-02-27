cfb31ec74cc8640368f8a58cd937973a8e4a00e9
Dariusz Walda <walda.darek@gmail.com>
2024-02-27 21:30:48 +0100

    challenge 36

---
+++ Training/Python/8kyu/26_50_exercises/36_find_numbers_which_are_divisible_by_given_number/find_numbers_which_are_divisible_by_given_number_best_solution.py
@@ -0,0 +1,18 @@
+def divisible_by(numbers, divisor):
+    return [x for x in numbers if x%divisor == 0]
+
+test1 = divisible_by([1, 2, 3, 4, 5, 6], 2)
+test2 = divisible_by([1, 2, 3, 4, 5, 6], 3)
+test3 = divisible_by([0, 1, 2, 3, 4, 5, 6], 4)
+test4 = divisible_by([0], 4)
+test5 = divisible_by([1, 3, 5], 2)
+test6 = divisible_by([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
+
+
+
+print(f"{test1} # [2, 4, 6]")
+print(f"{test2} # [3, 6]")
+print(f"{test3} # [0, 4]")
+print(f"{test4} # [0]")
+print(f"{test5} # []")
+print(f"{test6} # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]")
